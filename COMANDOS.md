2e49c34d0191c542195d701bef1c36c67eb8e88b
Luca <lgalafassi@fi.uba.ar>
2025-12-08 11:48:46 -0300

    Vinculada la base de datos con el backend!

--- COMANDOS.md
+++ COMANDOS.md
@@ -1,1 +1,6 @@
-psql -h localhost -U admin -d tpDb
+psql -h localhost -U admin -d tpDb -> acceder al postgres
+docker compose up -d -> levantar el postgres
+docker compose down -> Bajar el postgres
+sudo rm -rf VolumenBaseDatos1 -> Eliminar el volumen
+npm run dev -> Levantar la pagina
+
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import express from "express";
+import cors from "cors";
 import { pool } from "./db.js";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 // Ruta de prueba
-app.get("/", (req, res) => {
-  res.send("working");
+app.get("/usuarios", async (req, res) => {
+    try {
+        const result = await pool.query("SELECT * FROM usuarios");
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "DB error" });
+    }
 });
 
 const PORT = process.env.PORT || 3000;
