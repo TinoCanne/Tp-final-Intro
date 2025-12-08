9f1603af2800f9199c6cabc01a94093dc8679903
pascu04 <santinopb2004@gmail.com>
2025-12-08 15:22:20 -0300

    ahora si, aplicado el funcionamiento de la edicion de perfil

--- js/perfilUsuario.js
+++ js/perfilUsuario.js
@@ -1,9 +1,23 @@
 function editorPerfil(){
-                const editor = document.getElementById("formularioDeEdicion");
-                editor.style.display = "block"
-            }
-
-            document.addEventListener("DOMContentLoaded", function(){
-                const boton = document.getElementById("botonDeEdicion");
-                boton.onclick = editorPerfil;
+    document.addEventListener("DOMContentLoaded", function () {
+        const boton= document.getElementById("botonDeEdicion");
+        boton.onclick= function(){
+            const datos= document.querySelectorAll(".spanDatosPerfil");
+            datos.forEach(elemento => {
+                if (elemento.contentEditable === "true"){
+                    elemento.contentEditable = "false";
+                }
+                else{
+                    elemento.contentEditable = "true";
+                }
             })
+            if (boton.textContent === "Editar perfil"){
+                boton.textContent = "Guardar cambios";
+            }
+            else{
+                boton.textContent = "Editar perfil"
+            }
+        }
+    })
+}
+editorPerfil()
