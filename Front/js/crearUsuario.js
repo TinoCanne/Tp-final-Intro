c47f05116d1f522774c93c045056c5e806e1076f
agusgemetro <agusgemetro@gmail.com>
2025-12-10 14:51:21 -0300

    Arreglo de crear usuario js
    Added password confirmation validation and updated user creation function to include additional fields.

--- Front/js/crearUsuario.js
+++ Front/js/crearUsuario.js
@@ -19,14 +19,40 @@ function mostrar_contraseña(){
 }
 mostrar_contraseña();
 
-async function crear_usuario(event){
+function asegurar_contraseñas_iguales(event){
+    let contraseña = document.getElementById('contraseña_usuario');
+    let contraseña_repetida = document.getElementById('contraseña_usuario_repetida');
+    let err = document.getElementById('error_contraseña');
 
+    if (contraseña.value !== contraseña_repetida.value){
+        err.innerHTML = "Las contraseñas no coinciden.";
+        return false;
+    }
+    else{
+        err.innerHTML = "";
+        return true;
+    }
+}
+
+async function crear_usuario(event){
     event.preventDefault();
+
+    if (!asegurar_contraseñas_iguales(event)){
+        return
+    }
+
     try{
         const nombre = document.getElementById('nombre');
+        const apellido = document.getElementById('apellido');
         const username = document.getElementById('username');
+        const instrumentos = document.getElementById('instrumentos');
+        const generosFavoritos = document.getElementById('generosfavoritos');
+        const biografia = document.getElementById('biografia');
+        const redesSociales = document.getElementById('redessociales');
         const contraseña = document.getElementById('contraseña_usuario');
         const email = document.getElementById('email');
+        const linkfoto = document.getElementById('linkfoto');
+        const contacto = document.getElementById('contacto');
 
         const url = "http://localhost:3000/crear_usuario";
         const response = await fetch(url, {
@@ -36,9 +62,16 @@ async function crear_usuario(event){
             },
             body: JSON.stringify({
                 nombre: nombre.value,
+                apellido: apellido.value,
                 username: username.value,
                 contraseña: contraseña.value,
-                email: email.value
+                email: email.value,
+                instrumentos: instrumentos.value,
+                generosFavoritos: generosFavoritos.value,
+                biografia: biografia.value,
+                redesSociales: redesSociales.value,
+                linkFoto: linkfoto.value,
+                contacto: contacto.value
             })
         });
     }
@@ -46,4 +79,25 @@ async function crear_usuario(event){
         console.log(error);
     }
     event.target.reset();
+}
+
+function es_url_valido(url){
+    if (!url){
+        return false
+    }
+    return true
+}
+
+function mostrar_imagen_por_defecto(){
+    document.getElementById("vista_previa").src = "https://cdn-icons-png.flaticon.com/256/847/847969.png";
+}
+
+function mostrar_imagen() {
+    const url = document.getElementById("linkfoto").value;
+    const url_perfil_sin_foto = document.getElementById("vista_previa").src;
+    if (!es_url_valido(url)){
+        return
+    }
+    
+    document.getElementById("vista_previa").src = url;
 }
