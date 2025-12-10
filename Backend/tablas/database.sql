60c3064191ef75de99b8d40ad3c52f264fe7f0ba
TinoCanne <tinocanne@gmail.com>
2025-12-10 11:27:30 -0300

    actualice la base de datos en database.sql

--- Backend/tablas/database.sql
+++ Backend/tablas/database.sql
@@ -1,23 +1,8 @@
-CREATE TABLE usuarios (
-    id SERIAL PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    apellido VARCHAR (100),
-    username VARCHAR(100) NOT NULL,
-    contraseña VARCHAR(200) NOT NULL,
-    email VARCHAR(150) UNIQUE NOT NULL,
-    instrumentos VARCHAR(150),
-    generosFavoritos VARCHAR(150),
-    biografia VARCHAR(150),
-    redesSociales VARCHAR(150),
-    linkFoto VARCHAR(300),
-    contacto VARCHAR(15)
-);
-
 CREATE TABLE espacios (
     id SERIAL PRIMARY KEY,
     nombre VARCHAR(120) NOT NULL,
     ubicacion VARCHAR(150) NOT NULL,
-    fotos VARCHAR(250),
+    linkFotoEspacio VARCHAR(250),
     descripcion VARCHAR(150),
     contacto VARCHAR(100),
     horarios VARCHAR(150),
@@ -28,8 +13,47 @@ CREATE TABLE espacios (
 CREATE TABLE bandas (
     id SERIAL PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
-    generos VARCHAR(100),
     fechaCreacion INT,
     descripcion VARCHAR(150),
-    redesSociales VARCHAR(100)
+    redSocial VARCHAR(100)
+);
+
+CREATE TABLE usuarios (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    apellido VARCHAR (100),
+    username VARCHAR(100) NOT NULL,
+    contraseña VARCHAR(200) NOT NULL,
+    email VARCHAR(150) UNIQUE NOT NULL,
+    biografia VARCHAR(150),
+    redSocial VARCHAR(150),
+    linkFotoPerfil VARCHAR(300),
+    id_bandas INT,
+    FOREIGN KEY (id_bandas) REFERENCES bandas(id)
+);
+
+CREATE TABLE generos_usuarios (
+    id_usuario INT,
+    genero VARCHAR(30),
+    FOREIGN KEY (id_usuario) REFERENCES usuarios(id)
+);
+
+CREATE TABLE instrumentos (
+    id_usuario INT,
+    instrumento VARCHAR(30),
+    FOREIGN KEY (id_usuario) REFERENCES usuarios(id)
+);
+
+CREATE TABLE integrantes_bandas (
+    id_bandas INT,
+    id_integrante INT,
+    PRIMARY KEY (id_bandas, id_integrante),
+    FOREIGN KEY (id_bandas) REFERENCES bandas(id),
+    FOREIGN KEY (id_integrante) REFERENCES usuarios(id)
+);
+
+CREATE TABLE generos_bandas (
+    id_bandas INT,
+    genero VARCHAR(30),
+    FOREIGN KEY (id_bandas) REFERENCES bandas(id)
 );
