7bff1a71f2ef8485e10fe91cc976780fdd3d6032
Luca <lgalafassi@fi.uba.ar>
2025-12-10 15:01:52 -0300

    Arreglada la db

--- Backend/tablas/database.sql
+++ Backend/tablas/database.sql
@@ -1,17 +1,3 @@
-CREATE TABLE usuarios (
-    id SERIAL PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    apellido VARCHAR (100),
-    username VARCHAR(100) NOT NULL,
-    contraseña VARCHAR(200) NOT NULL,
-    email VARCHAR(150) UNIQUE NOT NULL,
-    instrumentos VARCHAR(150),
-    generosFavoritos VARCHAR(150),
-    biografia VARCHAR(150),
-    redesSociales VARCHAR(150),
-    linkFoto VARCHAR(300)
-);
-
 CREATE TABLE espacios (
     id SERIAL PRIMARY KEY,
     nombre VARCHAR(120) NOT NULL,
