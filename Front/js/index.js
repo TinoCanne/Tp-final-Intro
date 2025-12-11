56f1b95f914ac0d12d804cb0d8cdb03ec38cce55
Luca <lgalafassi@fi.uba.ar>
2025-12-11 16:44:09 -0300

    Revirtiendo errores

--- Front/js/index.js
+++ Front/js/index.js
@@ -1,8 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const botonBuscador = document.getElementById("botonBuscador");
     
-    const idUsuario = document.getElementById("idUsuario");
-
     const botonMusicos = document.getElementById('botonMostrarFiltrosMusicos');
     const botonBandas = document.getElementById('botonMostrarFiltrosBandas');
     const botonSalas = document.getElementById('botonMostrarFiltrosSalas');
@@ -61,6 +59,83 @@ document.addEventListener("DOMContentLoaded", () => {
 
 async function aplicar_filtros_musicos(event){
     event.preventDefault();
+    try{
+        const genero = document.getElementById('genero-musicos').value;
+        const instrumento = document.getElementById('instrumento').value;
+        const url = `http://localhost:3000/filtro_musicos?genero=${genero}&instrumento=${instrumento}`;
+        
+        const response = await fetch(url, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+        const data = await response.json();
+        console.log("Results:", data);
+    }
+    catch (error){
+        console.log(error);
+    }
+    event.target.reset();
+}
+
+async function aplicar_filtros_bandas(event){
+    event.preventDefault();
+    try{
+        const genero = document.getElementById('genero-bandas').value;
+        const url = `http://localhost:3000/filtro_bandas?genero=${genero}`;
+        
+        const response = await fetch(url, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+        const data = await response.json();
+        console.log("Results:", data);
+    }
+    catch (error){
+        console.log(error);
+    }
+    event.target.reset();
+}
+
+async function aplicar_filtros_espacios(event){
+    event.preventDefault();
+    try{
+        const ubicacion = document.getElementById('ubicacion').value;
+        const tamaño = document.getElementById('tamaño').value;
+        const precioPorHora = document.getElementById('precioPorHora').value;
+
+        const url = `http://localhost:3000/filtro_espacios?ubicacion=${ubicacion}&tamaño=${tamaño}&precioPorHora=${precioPorHora}`;
+        
+        const response = await fetch(url, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+        const data = await response.json();
+        console.log("Results:", data);
+    }
+    catch (error){
+        console.log(error);
+    }
+    event.target.reset();
+}
+
+async function aceptar_persona(){
+
+    const nombre = document.getElementById("nombre");
+    const bio = document.getElementById("bio");
+    const tags = document.getElementById("tags");
+    const foto = document.getElementById("foto");
+    const idPersona = document.getElementById("idPersona");
+    const id_persona_actual = parseInt(idPersona.value);
+    const url = `http://localhost:3000/usuarios/${id_persona_actual+1}`;
+
+    idPersona.value = toString(id_persona_actual+1);
+
     try{
         const genero = document.getElementById('genero-musicos').value;
         const instrumento = document.getElementById('instrumento').value;
