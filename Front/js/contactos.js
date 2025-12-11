47d319924c46e037b763ec2e2146085e5f5cfd41
TinoCanne <tinocanne@gmail.com>
2025-12-11 11:59:53 -0300

    commit temporal

--- Front/js/contactos.js
+++ Front/js/contactos.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const a = document.getElementById("botonEspacios");
 
     boton_usuarios.onclick = function(){
-        mostrar_usuarios(id_user);
+        cargar_contactos_usarios(id_user);
     };
     boton_bandas.onclick = function(){
         mostrar_bandas(id_user);
@@ -19,43 +19,45 @@ function test_botones(){
     console.log("click");
 }
 
-async function cargar_info_usuarios(id, path){
-    try{
-        const usuario = await fetch(`http://localhost:3000/${path}/${id}`, {
-            method: "GET",
-            headers:{
-                "Content-Type": "application/json",
-            },
-        });
-        const usuario_json = await usuario.json();
-        const marco = document.getElementById("marco_usuario");
-        const carta = document.createElement("div");
-        const foto = document.createElement("img");
-        const nombre = document.createElement("h3");
-        const contacto = document.createElement("p");
+function mostrar_usuarios(id){
+    const div_usuarios = document.getElementById("marco_usuario");
+    const div_espacios = document.getElementById("marco_espacios");
+    const div_bandas = document.getElementById("marco_bandas")
 
-        carta.className=("miniCarta");
-        foto.src = usuario_json.linkfotoperfil;
-        nombre.innerHTML = usuario_json.username;
-        contacto.innerHTML = usuario_json.contacto;
+    div_bandas.style.display = 'none';
+    div_espacios.style.display = 'none';
+    div_usuarios.style.display = 'block';
 
-        carta.appendChild(foto);
-        carta.appendChild(nombre)
-        carta.appendChild(contacto);
-        marco.appendChild(carta);
-
-        console.log(usuario_json)
-
-    }
-    catch(error){
-        console.log("DB error")
-    }
-    
+    cargar_contactos_usuarios(id);
 }
 
-async function cargar_info_bandas(id, path){
+function mostrar_bandas(id){
+    const div_usuarios = document.getElementById("marco_usuario");
+    const div_espacios = document.getElementById("marco_espacios");
+    const div_bandas = document.getElementById("marco_bandas")
+
+    div_bandas.style.display = "block";
+    div_espacios.style.display = "none";
+    div_usuarios.style.display = "none";
+
+    cargar_contactos(id, "bandas");
+}
+
+function mostrar_espacios(id){
+    const div_usuarios = document.getElementById("marco_usuario");
+    const div_espacios = document.getElementById("marco_espacios");
+    const div_bandas = document.getElementById("marco_bandas")
+
+    div_bandas.style.display = "none";
+    div_espacios.style.display = "block";
+    div_usuarios.style.display = "none";
+
+    cargar_contactos(id, "contactos_espacios");
+}
+
+async function cargar_info_bandas(id){
     try{
-        const banda = await fetch(`http://localhost:3000/${path}/${id}`, {
+        const banda = await fetch(`http://localhost:3000/bandas/${id}`, {
             method: "GET",
             headers:{
                 "Content-Type": "application/json",
@@ -88,76 +90,40 @@ async function cargar_info_bandas(id, path){
     }
 }
 
-async function cargar_info_espacios(id, path){
+async function cargar_info_espacios(id){
 
 }
 
-async function cargar_contactos(userId, path){
-    const url = `http://localhost:3000/${path}/${userId}`
-    console.log(url);
-    try{
-        const contactos = await fetch(url, {
+async function cargar_contactos_usarios(userId){
+    const url = `http://localhost:3000/contactos_usuarios/${userId}`;
+    const contactos = await fetch(url, {
             method: "GET",
-            headers: {
+            headers:{
                 "Content-Type": "application/json",
-            }
+            },
         });
-        const contactos_json = await contactos.json();
-        console.log(contactos_json);
+    contactos_json = await contactos.json();
+    contactos_json.forEach(contacto => {
+        const marco = document.getElementById("marco_usuario");
+        const carta = document.createElement("div");
+        const foto = document.createElement("img");
+        const nombre = document.createElement("h3");
+        const contacto = document.createElement("p");
 
-        if(path === "contactos_usuarios"){
-            contactos_json.forEach(contacto => {
-                cargar_info_usuarios(contacto.id_contacto_usuario, "usuarios")
-            });
-        }
-        else if(path === "contactos_bandas"){
-            contactos_json.forEach(contacto => {
-                cargar_info_bandas(contacto.id_contacto_bandas, "bandas")
-            });
-        }
-        else if(path === "contactos_espacios"){
-            contactos_json.forEach(contacto => {
-                cargar_info_espacios(contacto.id_contacto_espacio, "espacios")
-            });
-        }
-    }
-    catch(error){
-        console.log(error);
-    }
+        carta.className=("miniCarta");
+        foto.src = contacto.linkfotoperfil;
+        nombre.innerHTML = contacto.username;
+        contacto.innerHTML = contacto.contacto;
+
+        carta.appendChild(foto);
+        carta.appendChild(nombre)
+        carta.appendChild(contacto);
+        marco.appendChild(carta);
+
+    });
 }
 
-function mostrar_usuarios(id){
-    const div_usuarios = document.getElementById("marco_usuario");
-    const div_espacios = document.getElementById("marco_espacios");
-    const div_bandas = document.getElementById("marco_bandas")
+async function cargar_contactos_bandas(id){
+    const url = `http//localhost:3000/contactos_bandas/${id}`
 
-    div_bandas.style.display = "none";
-    div_espacios.style.display = "none";
-    div_usuarios.style.display = "block";
-
-    cargar_contactos(id, "contactos_usuarios");
-}
-
-function mostrar_bandas(id){
-    const div_usuarios = document.getElementById("marco_usuario");
-    const div_espacios = document.getElementById("marco_espacios");
-    const div_bandas = document.getElementById("marco_bandas")
-
-    div_bandas.style.display = "block";
-    div_espacios.style.display = "none";
-    div_usuarios.style.display = "none";
-
-    cargar_contactos(id, "bandas");
-}
-
-function mastrar_espacios(id){
-    const div_usuarios = document.getElementById("marco_usuario");
-    const div_espacios = document.getElementById("marco_espacios");
-    const div_bandas = document.getElementById("marco_bandas")
-
-    div_bandas.style.display = "none";
-    div_espacios.style.display = "block";
-    div_usuarios.style.display = "none";
-
-    cargar_contactos(id, "contactos_espacios");
 }
