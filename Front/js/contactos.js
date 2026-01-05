9ef5dc6c7e40f51edc352c215b602063586f64c3
tinoCanne <tinocanne@gmail.com>
2026-01-05 23:20:51 +0000

    commit pre arreglo del index.js de backend

--- Front/js/contactos.js
+++ Front/js/contactos.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function(){
     const boton_usuarios = document.getElementById("botonUsuarios");
     const boton_bandas = document.getElementById("botonBandas");
     const boton_espacios = document.getElementById("botonEspacios");
+    const id_usuario = localStorage.getItem('usuarioId')
     boton_usuarios.onclick = function(){
         mostrar_contactos_usuarios(1);
     }
@@ -17,49 +18,55 @@ function testBoton(num){
     console.log(`click ${num}`);
 }
 
-async function conseguir_data_usuario(user_id){
-    try{
-        const url = `http://localhost:3000/usuarios/${user_id}`;
-        const data_usuario = await fetch(url, {
-            method: "GET",
-            headers:{
-                "Content-Type": "application/json",
-            },
-        });
+async function armarCartaUsuario(id){
+    const url = `http://localhost:3000/usuarios/${id}`;
+    const container = document.getElementById("marco_usuario");
+    container.innerHTML = "";
+    const carta = document.createElement("div");
+    carta.className = "miniCarta";
 
-        const data_usuario_json = await data_usuario.json();
+    try{
+        const data_usuario = await fetch(url, {
+                method: "GET",
+                headers: {
+                        "Content-Type":"application/json",
+                }
+            }
+        )
+
+        const data_usuario_json = await data_usuario.json()
         console.log(data_usuario_json);
-        return data_usuario_json;
+
+
+        const foto_usuario = document.createElement("img");
+        foto_usuario.src = data_usuario_json.linkFotoPerfil;
+
+        const nombre = document.createElement("p");
+        nombre.textContent = data_usuario_json.nombre;
+        carta.appendChild(foto_usuario);
+        carta.appendChild(nombre);
+        container.appendChild(carta);
+
     }
     catch(err){
         console.log(err);
     }
 }
 
+
 async function mostrar_contactos_usuarios(id_usuario){
     const url = `http://localhost:3000/pedir_contactos/${id_usuario}`
-    try{
-        const contactos = await fetch(url, {
-            method: "GET",
-            headers: {
-                "Content-Type":"application/json",
-            },
-        });
-        const contactos_json = await contactos.json();
-        const marco_usuarios = document.getElementById("marco_usuario");
-        marco_usuarios.innerHTML = "";
-        contactos_json.forEach(contacto => {
-
-            const data = conseguir_data_usuario(contacto.id_contacto_usuario);
-
-            const carta_contacto = document.createElement("div");
-            carta_contacto.className = "miniCarta";
-            marco_usuarios.appendChild(carta_contacto);
-        });
-    }
-    catch(err){
-        console.log(err);
-    }
+    const contactos = await fetch(url, {
+        method: "GET",
+        headers: {
+            "Content-Type":"application/json",
+        },
+    });
+    const contactos_json = await contactos.json();
+    contactos_json.forEach(contacto => {
+        armarCartaUsuario(contacto.id_contacto_usuario);
+    });
+        
 }
 
 function mostrar_contactos_bandas(id_usuario){
