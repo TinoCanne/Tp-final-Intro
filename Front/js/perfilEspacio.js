ec0febea648fc2234082a84e379b0a69ba0381bd
Santino <santinopb2004@gmail.com>
2026-01-22 05:30:11 -0300

    agregado calendario funcionl con el boton de reservas

--- Front/js/perfilEspacio.js
+++ Front/js/perfilEspacio.js
@@ -1,4 +1,6 @@
-async function armarCartaEstudio(){
+
+
+document.addEventListener("DOMContentLoaded", async function(){
     const url = `http://localhost:3000/espacios/`;
     const container = document.getElementById("marco_estudios");
     container.innerHTML = "";
@@ -33,7 +35,7 @@ async function armarCartaEstudio(){
             botonReserva.textContent = "Reservar";
             botonReserva.className = "botonReservar";
             botonReserva.id = "botonReservar";
-
+            botonReserva.onclick = mostrarCalendario;
 
             carta.appendChild(fotoEstudio);
             carta.appendChild(nombre);
@@ -49,6 +51,88 @@ async function armarCartaEstudio(){
     catch(err){
         console.log(err);
     }
+})
+
+function mostrarCalendario(){
+    let calendario = document.getElementById('cuadro_reservas');
+    let fondo = document.getElementById('fondo_reservas');
+    calendario.style.display = 'flex';
+    fondo.style.display = 'block';
 }
 
-armarCartaEstudio();
+function cerrarCalendario(){
+    let calendario = document.getElementById('cuadro_reservas')
+    let fondo = document.getElementById('fondo_reservas');
+    calendario.style.display = 'none';
+    fondo.style.display = 'none';
+}
+
+let hoy = new Date();
+let hoyParaComparacion = hoy.setHours(0, 0, 0, 0);
+function armarCalendario(ano, mes, hoyParaComparacion){
+    let meses = Array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
+    let ubicacionCalendario = document.getElementById('ubicacion_en_calendario');
+    let contenidoCalendario = document.getElementById('contenido_calendario');
+    
+    let primerDiaMesSemana = (new Date(ano, mes, 1)).getDay();
+    if (primerDiaMesSemana === 0) {    //en js el 0 significa domingo, no lunes.
+        primerDiaMesSemana = 7;
+    }
+    let ultimoDiaMes = (new Date(ano, mes+1, 0)).getDate();
+    let ultimaCeldaMes = (primerDiaMesSemana + ultimoDiaMes - 1);
+    let cantidadCeldas = 42;
+    console.log(ultimaCeldaMes);
+    if (ultimaCeldaMes <= 35){
+        cantidadCeldas = 35;
+    }
+
+    let contenidoTemporal = "";
+    let diaTemporal = 0;
+    for (let i = 1; i <= cantidadCeldas; i++){  //empieza en 1 porque el primerDiaMes minimo es 1.
+        if ((i % 7) === 1 ) {
+            contenidoTemporal += "<tr>";
+        }
+        if ((i < primerDiaMesSemana) || (i > ultimaCeldaMes)){
+            contenidoTemporal += "<td> </td>";
+        }
+        else{
+            if (i === primerDiaMesSemana){
+                diaTemporal = 1;
+            }
+            let fechaCelda = new Date(ano, mes, diaTemporal);
+            if (fechaCelda < hoyParaComparacion){
+                contenidoTemporal += "<td class='diasAnteriores'>" + diaTemporal + "</td>";
+            }
+            else{
+                contenidoTemporal += "<td class='diasPosteriores' onclick=''>" + diaTemporal + "</td>";
+            }
+            diaTemporal++;
+        }
+
+        if ((i % 7) === 0){
+            contenidoTemporal += "</tr>"
+        }
+    }
+
+    contenidoCalendario.innerHTML = contenidoTemporal;
+
+    let proximoMes = mes + 1;
+    let mesAnterior = mes -1
+    let proximoAno = ano;
+    let anoAnterior = ano;
+
+    if (proximoMes === 12){
+        proximoMes = 0;
+        proximoAno = ano + 1;
+    }
+
+    if (mesAnterior === -1){
+        mesAnterior = 11
+        anoAnterior = ano - 1;
+    }
+
+    ubicacionCalendario.innerHTML = "<button onclick='armarCalendario(" + anoAnterior +", " + mesAnterior + ", " + hoyParaComparacion + ")'>&#171</button> <div>" + meses[mes] + "/" + ano + "</div> <button class='botonDerecha' onclick='armarCalendario(" + proximoAno + ", " + proximoMes + ", " + hoyParaComparacion + ")'>&#187</button>";
+
+}
+
+armarCalendario(hoy.getFullYear(), hoy.getMonth(), hoyParaComparacion);
