ad5d78c659f7fc5d26248c3e7081489e7535d0a3
Luca <lgalafassi@fi.uba.ar>
2026-01-30 19:25:27 -0300

    Modificado el metodo post de espacios

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -379,7 +379,7 @@ app.post("/espacios", async (req, res) => {
     try{
         const precio = parseInt(req.body.precio);
         const idUsuario = parseInt(req.body.idUsuario);
-        const query_espacio = `INSERT INTO espacios (nombre, ubicacion, linkfotoespacio, descripcion, contacto, tamaño, precioPorHora, id_dueño) VALUES ('${req.body.nombre}', '${req.body.ubicacion}', '${req.body.linkfotoespacio}', '${req.body.descripcion}', '${req.body.contacto}', '${req.body.tamaño}', ${precio}, ${idUsuario})`;
+        const query_espacio = `INSERT INTO espacios (nombre, ubicacion, linkfotoespacio, descripcion, contacto, tamaño, precioPorHora, id_dueño, diasAbierto, horarioCierre, horarioApertura) VALUES ('${req.body.nombre}', '${req.body.ubicacion}', '${req.body.linkfotoespacio}', '${req.body.descripcion}', '${req.body.contacto}', '${req.body.tamaño}', ${precio}, ${idUsuario}, ${req.body.diasAbiertos}, ${req.body.horarioCierre}, ${req.body.horarioApertura})`;
         await pool.query(query_espacio);
         
         res.json({ message: "Espacio creado" });  
